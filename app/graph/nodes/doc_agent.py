98b3f46c2a74d4aebf96e47c47380b380e6ba206
hadmom1201 <hadmom1201@gmail.com>
2026-04-21 14:39:13 +0500

    Update doc_agent.py

--- app/graph/nodes/doc_agent.py
+++ app/graph/nodes/doc_agent.py
@@ -9,7 +9,7 @@ import os
 # =========================================================
 
 gemini_llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
+    model="gemini-2.0-flash-ex",
     temperature=0.3,
     api_key=os.getenv("GOOGLE_API_KEY")
 )
