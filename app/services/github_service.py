7e2c026c0b2a6d10e9b4ba9efb01d9d816d797d2
hadmom1201 <hadmom1201@gmail.com>
2026-05-06 21:27:34 +0500

    Update github_service.py

--- app/services/github_service.py
+++ app/services/github_service.py
@@ -15,7 +15,11 @@ async def fetch_user_repos(access_token: str):
     return res.json()
 
 
+# =========================
+# RECURSIVE FILE FETCH (IMPORTANT)
+# =========================
 async def fetch_repo_contents(access_token: str, owner: str, repo: str, path=""):
+
     headers = {
         "Authorization": f"Bearer {access_token}",
         "Accept": "application/vnd.github+json"
@@ -26,4 +30,27 @@ async def fetch_repo_contents(access_token: str, owner: str, repo: str, path="")
     async with httpx.AsyncClient() as client:
         res = await client.get(url, headers=headers)
 
-    return res.json()
+    data = res.json()
+
+    result = []
+
+    for item in data:
+
+        # folder → recurse
+        if item["type"] == "dir":
+            sub = await fetch_repo_contents(
+                access_token,
+                owner,
+                repo,
+                item["path"]
+            )
+            result.extend(sub)
+
+        # file → include content
+        elif item["type"] == "file":
+            result.append({
+                "path": item["path"],
+                "url": item["html_url"]
+            })
+
+    return result
