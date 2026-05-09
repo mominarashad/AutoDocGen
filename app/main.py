a26f5d48b224f6c6f5b1caf3d15978cf42b247de
hadmom1201 <hadmom1201@gmail.com>
2026-05-09 16:05:41 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -46,7 +46,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://autodocgen-production.up.railway.app"
+        "https://autodocgen-production.up.railway.app",
+         "http://127.0.0.1:5173"
     ],
     allow_credentials=True,
     allow_methods=["*"],
