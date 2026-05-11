0eb004d1b762ddf13422855491926a524ed8faf5
hadmom1201 <hadmom1201@gmail.com>
2026-05-11 13:40:33 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -74,6 +74,8 @@ from app.routes.github_webhook_router import router as github_webhook_router
 from app.routes.notification_router import router as notification_router
 from app.routes.subscription_router import router as subscription_router
 from app.routes.workspace_router import router as workspace_router
+from app.routes.admin_router import router as admin_router
+app.include_router(admin_router)
 
 
 app.include_router(subscription_router)
